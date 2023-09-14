e618d6452d39265e911d3823e73e84071a99bfd5
Tatu Soininen <tatu.soininen@cinia.fi>
2023-09-14 15:34:24 +0300

    Removed irrelevant characters

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfigOAuth2.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfigOAuth2.java
@@ -14,7 +14,7 @@ public class SpringSecurityConfigOAuth2 {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/v3/api-docs", "/v3/api-docs/**",gi "/swagger-ui/**", "/swagger-ui.html")
+                .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().oauth2ResourceServer().jwt();
