3c48bcdaafa4ddf35fc228d80d0796e8b59c38a0
Tatu Soininen <tatu.soininen@cinia.fi>
2023-09-15 09:42:25 +0300

    Configure REST-assured to bypass authentication on test profile

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfigBasicAuthentication.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfigBasicAuthentication.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Profile("basic")
+@Profile({"basic","!test"})
 @Configuration
 public class SpringSecurityConfigBasicAuthentication {
 
